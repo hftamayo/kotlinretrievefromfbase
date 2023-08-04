29bb53731d95f6cc2e18c1ceb47c7e9eb19136fc
hftamayo <hftamayo@gmail.com>
2023-08-04 10:08:20 -0600

    using var keyword for constructor

--- app/src/main/java/com/hftamayo/kotlinretrievefromfbase/Task.kt
+++ app/src/main/java/com/hftamayo/kotlinretrievefromfbase/Task.kt
@@ -1,3 +1,3 @@
 package com.hftamayo.kotlinretrievefromfbase
 
-data class Task (private val text: String, private val completed: Boolean)
+data class Task (private var text: String, private var completed: Boolean)
