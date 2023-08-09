51d2f78d6213cd7918d50df2438ccf17f6650bdd
hftamayo <hftamayo@gmail.com>
2023-08-09 10:57:46 -0600

    init the View and its related objects

--- app/src/main/java/com/hftamayo/kotlinretrievefromfbase/MainActivity.kt
+++ app/src/main/java/com/hftamayo/kotlinretrievefromfbase/MainActivity.kt
@@ -1,62 +1,166 @@
 package com.hftamayo.kotlinretrievefromfbase
 
+import android.app.ProgressDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.annotation.Nullable
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.FirebaseException
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var recyclerView: RecyclerView
-    private lateinit var userArrayList: ArrayList<User>
-    private var myAdapter: MyAdapter? = null
+    private lateinit var usersArrayList: ArrayList<User>
+    private var userAdapter : UserDataAdapter? = null
     private var db: FirebaseFirestore? = null
-    private var progressDialog? = ProgressDialog
-    override fun onCreate(savedInstanceState: Bundle?) {
+    private var progressDialog: ProgressDialog? = null
+
+    override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        progressDialog = new ProgressDialog(this);
-        progressDialog.setCancelable(false)
-        progressDialog.setMessage("Fetching data...")
-        progressDialog.show()
+        initView()
+        initRecyclerView()
+    }
 
-        recyclerView = findViewById<R.id.recyclerView>()
-        recyclerView.setHasFixedSize(true)
-        recyclerView.setLayoutManager(new LinearLayoutManager(this))
-
+    private fun initView(){
+        recyclerView = findViewById(R.id.recyclerView)
         db = FirebaseFirestore.getInstance()
-        userArrayList = new ArrayList<User>()
-        myAdapter = new MyAdapter(MainActivity.this.userArrayList)
+    }
 
-        recyclerView.setAdapter(myAdapter)
-
-        EventChangeListener()
+    private fun initRecyclerView(){
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        userAdapter = UserDataAdapter()
+        recyclerView.adapter = userAdapter
 
     }
 
-    private void EventChangeListener() {
-        db.collection("users").orderBy("firstName", Query.Direction.ASCENDING)
-            .addSnapshotListener(new EventListener<QuerySnapshot>(){
-                @Override
-                public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error){
-                    if(error != null){
-                        if(progressDialog.isShowing())
-                            progressDialog.dismiss()
-                        Log.e("firestore error", error.getMessage())
-                        return
-                    }
-                    for (DocumentChange dc : value.getDocumentChanges()){
-                        if(dc.getType() == DocumentChange.Type.ADDED){
-                            userArrayList.add(dc.getDocument().toObject(User.class))
-                        }
-                    myAdapter.notifyDataSetChanged()
-                    if(progressDialog.isShowing())
-                        progressDialog.dismiss()
-                }
 
-                }
-            });
-    }
+
+
+
+
+
+//    private lateinit var edName: EditText
+//    private lateinit var edEmail: EditText
+//    private lateinit var btnAdd: Button
+//    private lateinit var btnView: Button
+//    private lateinit var btnUpdate: Button
+//
+//    private lateinit var sqLiteHelper: SQLiteHelper
+////    private lateinit var recyclerView: RecyclerView
+//    private var adapter: StudentAdapter? = null
+//    private var std: StudentModel? = null
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//
+//        initView()
+//        initRecyclerView()
+//        sqLiteHelper = SQLiteHelper(this)
+//        btnAdd.setOnClickListener { addStudent() }
+//        btnView.setOnClickListener{ getStudents() }
+//        btnUpdate.setOnClickListener{ updateStudent() }
+//
+//        adapter?.setOnClickItem { Toast.makeText(this, it.name, Toast.LENGTH_SHORT).show()
+//            edName.setText(it.name)
+//            edEmail.setText(it.email)
+//            std = it
+//        }
+//        adapter?.setOnClickDeleteItem {
+//            deleteStudent(it.id)
+//
+//        }
+//    }
+//
+//    private fun getStudents(){
+//        val stdList = sqLiteHelper.getAllStudent()
+//        Log.e("testing:", "${stdList.size}")
+//        adapter?.addItems(stdList)
+//    }
+//
+//    private fun addStudent(){
+//        val name = edName.text.toString()
+//        val email = edEmail.text.toString()
+//
+//        if(name.isEmpty() || email.isEmpty()){
+//            Toast.makeText(this, "Please enter required field", Toast.LENGTH_SHORT).show()
+//        }else {
+//            val std = StudentModel(name = name, email = email)
+//            val status = sqLiteHelper.insertStudent(std)
+//            //checking for the status of the operation
+//            if(status > -1 ){
+//                Toast.makeText(this, "Student Added!", Toast.LENGTH_SHORT ).show()
+//                clearEditText()
+//                getStudents()
+//            } else {
+//                Toast.makeText(this, "An error ocurred during record insertion", Toast.LENGTH_SHORT).show()
+//            }
+//        }
+//    }
+//
+//    private fun updateStudent(){
+//        val name = edName.text.toString()
+//        val email = edEmail.text.toString()
+//
+//        if(name == std?.name && email == std?.email){
+//            Toast.makeText(this, "Record not changed...", Toast.LENGTH_SHORT).show()
+//            return
+//        }
+//        if(std == null) return
+//
+//        val std = StudentModel(id = std!!.id, name = name, email = email)
+//        val status = sqLiteHelper.updateStudent(std)
+//        if(status > -1){
+//            clearEditText()
+//            getStudents()
+//        } else {
+//            Toast.makeText(this, "Update failed", Toast.LENGTH_SHORT).show()
+//        }
+//    }
+//
+//    private fun deleteStudent(id:Int){
+//        val builder = AlertDialog.Builder(this)
+//        builder.setMessage("Are you sure you want to delete item?")
+//        builder.setCancelable(true)
+//        builder.setPositiveButton("Yes"){ dialog, _ ->
+//            sqLiteHelper.deleteStudentById(id)
+//            getStudents()
+//            dialog.dismiss()
+//        }
+//        builder.setNegativeButton("No"){ dialog, _ ->
+//            dialog.dismiss()
+//        }
+//        val alert = builder.create()
+//        alert.show()
+//    }
+//
+//    private fun clearEditText(){
+//        edName.setText("")
+//        edEmail.setText("")
+//        edName.requestFocus()
+//    }
+//
+//    private fun initRecyclerView(){
+//        recyclerView.layoutManager = LinearLayoutManager(this)
+//        adapter = StudentAdapter()
+//        recyclerView.adapter = adapter
+//
+//    }
+//
+//    private fun initView(){
+//        edName = findViewById(R.id.edName)
+//        edEmail = findViewById(R.id.edEmail)
+//        btnAdd = findViewById(R.id.btnAdd)
+//        btnView = findViewById(R.id.btnView)
+//        btnUpdate = findViewById(R.id.btnUpdate)
+//        recyclerView = findViewById(R.id.recyclerView)
+//    }
+
 }
