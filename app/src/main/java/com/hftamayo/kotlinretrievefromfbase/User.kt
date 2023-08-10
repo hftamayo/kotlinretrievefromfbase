4a7eb1d5d09aa73e0e9cfd43ed3e4df94164e9d6
hftamayo <hftamayo@gmail.com>
2023-08-10 09:00:24 -0600

    adding properties suggested by firebase

--- app/src/main/java/com/hftamayo/kotlinretrievefromfbase/User.kt
+++ app/src/main/java/com/hftamayo/kotlinretrievefromfbase/User.kt
@@ -1,3 +1,10 @@
 package com.hftamayo.kotlinretrievefromfbase
 
-data class User(var firstName: String, var lastName: String, var age: Int)
+import com.google.firebase.database.IgnoreExtraProperties
+
+@IgnoreExtraProperties
+data class User(var firstName: String? = null, var lastName: String? = null, var age: Int? = null){
+    // Null default values create a no-argument default constructor, which is needed
+    // for deserialization from a DataSnapshot.
+
+}
