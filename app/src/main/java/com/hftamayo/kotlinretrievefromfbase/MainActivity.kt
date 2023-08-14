ad1e49e4ba8ef8dc06144e610416d024b80e2048
hftamayo <hftamayo@gmail.com>
2023-08-14 11:16:02 -0600

    new version of getData function as an alternative for testing connection to firebase

--- app/src/main/java/com/hftamayo/kotlinretrievefromfbase/MainActivity.kt
+++ app/src/main/java/com/hftamayo/kotlinretrievefromfbase/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.hftamayo.kotlinretrievefromfbase
 
 import android.app.ProgressDialog
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.database.DataSnapshot
@@ -10,9 +13,12 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreSettings
 
 class MainActivity : AppCompatActivity() {
-    var databaseReference: DatabaseReference? = null
+    private var db : FirebaseFirestore? = null
+    private var databaseReference : DatabaseReference? = null
     var eventListener : ValueEventListener? = null
     private lateinit var recyclerView: RecyclerView
     private lateinit var usersArrayList: ArrayList<User>
@@ -47,9 +53,41 @@ class MainActivity : AppCompatActivity() {
         progressDialog!!.show()
     }
 
+    private fun getData2(){
+        usersArrayList = ArrayList()
+        db = FirebaseFirestore.getInstance()
+        db!!.firestoreSettings = FirebaseFirestoreSettings.Builder().build()
+
+        db!!
+            .collection("users")
+            .get()
+            .addOnSuccessListener { data ->
+                try{
+                    if (data != null){
+                        for(record in data) {
+                            val fName = record["firstName"].toString()
+                            val lName = record["lastName"].toString()
+                            val age = record["age"].toString().toInt()
+                            val user = User(firstName = fName, lastName = lName, age = age)
+                            usersArrayList.add(user)
+                        }
+                        userAdapter.notifyDataSetChanged()
+                        //Toast.makeText(context, "Data read successfully", Toast.LENGTH_LONG).show()
+                    } else {
+                        //Toast.makeText(context, "No data found", Toast.LENGTH_LONG).show()
+                    }
+                }catch (ex: Exception) {
+                    ex.message?.let { Log.e(TAG, it) }
+                }
+            }.addOnFailureListener{
+                e -> Log.e(TAG, "Error in the connection process", e)
+            }
+
+    }
+
     private fun getData(){
         usersArrayList = ArrayList()
-        databaseReference = FirebaseDatabase.getInstance().getReference("todo")
+        databaseReference = FirebaseDatabase.getInstance().getReference("users")
         eventListener = databaseReference!!.addValueEventListener(object: ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
                 usersArrayList.clear()
