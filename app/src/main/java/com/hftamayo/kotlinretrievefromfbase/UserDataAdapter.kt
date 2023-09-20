846853db9dc8994dd42197f48488b7643935165e
hftamayo <hftamayo@gmail.com>
2023-09-20 12:47:56 -0600

    refactoring dataAdapter with chatGPT suggestions

--- app/src/main/java/com/hftamayo/kotlinretrievefromfbase/UserDataAdapter.kt
+++ app/src/main/java/com/hftamayo/kotlinretrievefromfbase/UserDataAdapter.kt
@@ -5,40 +5,29 @@ import android.widget.TextView
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import org.w3c.dom.Text
 
-class UserDataAdapter: RecyclerView.Adapter<UserDataAdapter.UserViewHolder>() {
-    private var userArrayList: ArrayList<User> = ArrayList()
-
-    fun addItems(items: ArrayList<User>){
-        this.userArrayList = items
-        notifyDataSetChanged()
+class UserDataAdapter(private val itemList: List<User>): RecyclerView.Adapter<UserDataAdapter.ViewHolder>() {
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val firstName: TextView = itemView.findViewById(R.id.tvFirstName)
+        val lastName: TextView = itemView.findViewById(R.id.tvLastName)
+        val age: TextView = itemView.findViewById(R.id.tvAge)
     }
 
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ) = UserViewHolder (
-        LayoutInflater.from(parent.context).inflate(R.layout.item, parent, false)
-    )
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item, parent, false)
+        return ViewHolder(view)
+    }
 
-    override fun onBindViewHolder(holder: UserViewHolder, position: Int){
-        val userItem = userArrayList[position]
-        holder.bindView(userItem)
+    override fun onBindViewHolder(holder: ViewHolder, position: Int){
+        val userItem = itemList[position]
+        holder.firstName.text = userItem.firstName
+        holder.lastName.text = userItem.lastName
+        holder.age.text = userItem.age.toString()
     }
 
     override fun getItemCount(): Int {
-        return userArrayList.size
-    }
-
-    class UserViewHolder(view: View) : RecyclerView.ViewHolder(view){
-        private var firstName = view.findViewById<TextView>(R.id.tvFirstName)
-        private var lastName = view.findViewById<TextView>(R.id.tvLastName)
-        private var age = view.findViewById<TextView>(R.id.tvAge)
-        fun bindView(std:User){
-            firstName.text = std.firstName
-            lastName.text = std.lastName
-            age.text = std.age.toString()
-        }
+        return itemList.size
     }
 
 }
